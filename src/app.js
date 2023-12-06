e57c3ba47c6e9b3c4b5f7f80ef450ba7fcab7ad0
AgusAllend <148511275+AgusAllend@users.noreply.github.com>
2023-12-06 01:00:02 -0300

    ultimo commit Mariano y Agustín

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
 app.listen(3060, () => console.log('El servidor esta corriendo en: http://localhost:3060'));
-
 app.get("/",function (req,res) {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
-});
+})
 
 app.get("/detalle",function (req,res) {
     res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
@@ -17,4 +16,12 @@ app.get("/detalle",function (req,res) {
 app.get("/productCart", function (req,res) {
     res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
 });
-
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+app.post('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+app.get("/login",function (req,res) {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
